ed27fc3a0d15766e5ceba4abc517bfd88c892596
Sergey Besedin <kr3ssh@pm.me>
2019-07-28 13:48:29 +0400

    Refactor main.go

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,8 @@ RUN go get \
   github.com/requilence/integram \
   github.com/integram-org/gitlab \
   github.com/integram-org/trello \
-  github.com/integram-org/webhook
+  github.com/integram-org/webhook \
+  github.com/kelseyhightower/envconfig
 
 COPY run.sh /
 COPY main.go /
--- main.go
+++ main.go
@@ -4,38 +4,32 @@ import (
 	"github.com/integram-org/gitlab"
 	"github.com/integram-org/trello"
 	"github.com/integram-org/webhook"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/requilence/integram"
-	"os"
 )
 
 func main() {
-	integram.Debug = true
-
-	integram.Register(
-		trello.Config{
-			integram.OAuthProvider{
-				ID:     os.Getenv("TRELLO_APP_KEY"),
-				Secret: os.Getenv("TRELLO_APP_SECRET"),
-			},
-		},
-		os.Getenv("TRELLO_BOT_TOKEN"),
-	)
-
-	integram.Register(
-		gitlab.Config{
-			integram.OAuthProvider{
-				ID:     os.Getenv("GITLAB_APP_ID"),
-				Secret: os.Getenv("GITLAB_APP_SECRET"),
-			},
-		},
-		os.Getenv("GITLAB_BOT_TOKEN"),
-	)
-
+	var trelloCfg trello.Config
+	var gitlabCfg gitlab.Config
 	var webhookCfg webhook.Config
+
+	envconfig.MustProcess("TRELLO", &trelloCfg)
+	envconfig.MustProcess("GITLAB", &gitlabCfg)
+	envconfig.MustProcess("WEBHOOK", &webhookCfg)
+
+	integram.Register(
+		trelloCfg,
+		trelloCfg.BotConfig.Token,
+	)
+
+	integram.Register(
+		gitlabCfg,
+		gitlabCfg.BotConfig.Token,
+	)
 
 	integram.Register(
 		webhookCfg,
-		os.Getenv("WEBHOOK_BOT_TOKEN"),
+		webhookCfg.BotConfig.Token,
 	)
 
 	integram.Run()
